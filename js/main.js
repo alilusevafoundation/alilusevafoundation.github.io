b71d5fbed6960045417c9ecfaddd2608629d985d
Santosh Parashetti <sparashetti@kabbage.com>
2019-08-08 16:56:23 +0530

    Add donation QR/UPI

--- js/main.js
+++ js/main.js
@@ -32,7 +32,25 @@ $(document).ready(function(){
 
   $('.nav-item a:first').tab('show');
 
-
+  $('.cd-popup-trigger').on('click', function(event){
+		event.preventDefault();
+		$('.cd-popup').addClass('is-visible');
+	});
+	
+	//close popup
+	$('.cd-popup').on('click', function(event){
+		if( $(event.target).is('.cd-popup-close') || $(event.target).is('.cd-popup') ) {
+			event.preventDefault();
+			$(this).removeClass('is-visible');
+		}
+  });
+  
+	//close popup when clicking the esc keyboard button
+	$(document).keyup(function(event){
+    if(event.which=='27'){
+      $('.cd-popup').removeClass('is-visible');
+    }
+  });
 
   // Select all links with hashes
   $('.main-menubar a[href*="#"]')
